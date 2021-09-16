9b1c1c11e9695bf1559c98520984d8736e4d8162
Aaqa Ishtyaq <aaqaishtyaq@gmail.com>
2021-09-17 00:54:13 +0530

    add colors, sensible vcs config
    Signed-off-by: Aaqa Ishtyaq <aaqaishtyaq@gmail.com>

---
+++ src/venv.rs
@@ -0,0 +1,32 @@
+use std::env;
+use colored::*;
+use std::path::Path;
+
+pub fn get_name() -> colored::ColoredString {
+    match env::var("VIRTUAL_ENV") {
+        Ok(venv_path) => {
+            let venv_name = Path::new(&venv_path[..]).file_name();
+            if let Some(name) = venv_name {
+                if let Some(valid_name) = name.to_str() {
+                    return format!("({})", valid_name).bright_black();
+                }
+            }
+        }
+        Err(_) => {}
+    }
+    return "".white()
+}
+
+pub fn in_nix_shell() -> colored::ColoredString {
+    match env::var("IN_NIX_SHELL") {
+        Ok(p) => {
+            if p == "pure" {
+                "(nix) ".green()
+            } else {
+                "(nix) ".red()
+            }
+        }
+        _ => return "".white(),
+    }
+}
+
