699baaa64392731ed940d0726f0ef39b32adfbc6
Aaqa Ishtyaq <aaqaishtyaq@gmail.com>
2022-01-05 19:27:56 +0530

    fix: escape % for zsh, set $ as default for bash
      * set `%` as default prommpt character for zsh.
      * set `$` as default prompt character for bash.
      * escape `%` if shell is zsh.
    
    Signed-off-by: Aaqa Ishtyaq <aaqaishtyaq@gmail.com>

--- src/prompt_char.rs
+++ src/prompt_char.rs
@@ -5,8 +5,15 @@ Copyright (C) 2021 Aaqa Ishtyaq
 use iay::colors;
 use std::env;
 
-pub fn prompt_char() -> String {
-    let user_char = env::var("IAY_PROMPT_CHAR").unwrap_or_else(|_| "%".into());
+pub fn prompt_char(zsh: bool) -> String {
+    let shell_user_char = if zsh {
+        // % is a special char in zsh, escape it.
+        "%#"
+    } else {
+        "$"
+    };
+
+    let user_char = env::var("IAY_PROMPT_CHAR").unwrap_or_else(|_| shell_user_char.into());
     let root_char = env::var("IAY_PROMPT_CHAR_ROOT").unwrap_or_else(|_| "#".into());
     let user_char_color = env::var("IAY_PROMPT_CHAR_COLOR").unwrap_or_else(|_| "blue".into());
     let root_char_color = env::var("IAY_PROMPT_CHAR_ROOT_COLOR").unwrap_or_else(|_| "red".into());
